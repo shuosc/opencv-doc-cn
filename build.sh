37b7938173376da85b6dfaa765ee3e5e91809995
Yiqun Zhao <cosformula@qq.com>
2018-07-01 11:52:55 +0800

    build script

--- build.sh
+++ build.sh
@@ -7,4 +7,4 @@ find . -a -name "*.markdown" -not -name "*cn.markdown" | xargs rm -f
 find .  -name "*.jpg" -o -name "*.png" -o -name "*.bmp" | xargs -n1 -I {} cp {} ./images
 doxygen Doxyfile.in
 git checkout gh-pages
-cp ./build/build/html/ ./
+cp -r build/html/ ./
