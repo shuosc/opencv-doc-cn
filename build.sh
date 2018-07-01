8503190e60e4e6729433cea50b81e90c9f50d167
Yiqun Zhao <cosformula@qq.com>
2018-07-01 11:40:38 +0800

    build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+git checkout master
 mkdir build
 cd build
 cp ../* .
@@ -5,3 +6,5 @@ cp -r ../py_tutorials .
 find . -a -name "*.markdown" -not -name "*cn.markdown" | xargs rm -f
 find .  -name "*.jpg" -o -name "*.png" -o -name "*.bmp" | xargs -n1 -I {} cp {} ./images
 doxygen Doxyfile.in
+git checkout gh-pages
+cp ./build/build/html/* .
