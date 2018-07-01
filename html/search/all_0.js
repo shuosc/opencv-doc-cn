218cc92b31d89f5bf87351e68c06c6af199a22c0
Yiqun Zhao <cosformula@qq.com>
2018-07-01 11:41:58 +0800

    update

---
+++ html/search/all_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['brief特征点描述算法_5b_5e1_5d',['BRIEF特征点描述算法[^1]',['../de/db0/tutorial_py_brief_cn.html',1,'tutorial_py_table_of_contents_feature2d_cn']]]
+];
---
+++ html/search/pages_7.js
@@ -0,0 +1,13 @@
+var searchData=
+[
+  ['二维直方图',['二维直方图',['../d7/d56/tutorial_py_2d_histogram_cn.html',1,'tutorial_py_table_of_contents_histograms_cn']]],
+  ['使用_20haar_20cascades_20的面部识别',['使用 Haar Cascades 的面部识别',['../da/ddc/tutorial_py_face_detection_cn.html',1,'tutorial_py_table_of_contents_objdetect_cn']]],
+  ['使用grabcut算法交互式前景提取',['使用GrabCut算法交互式前景提取',['../d2/d24/tutorial_py_grabcut_cn.html',1,'tutorial_py_table_of_contents_imgproc_cn']]],
+  ['使用knn进行手写字符的ocr',['使用kNN进行手写字符的OCR',['../d9/de9/tutorial_py_knn_opencv_cn.html',1,'tutorial_py_knn_index_cn']]],
+  ['了解k_2d最近邻算法',['了解k-最近邻算法',['../d7/dac/tutorial_py_knn_understanding_cn.html',1,'tutorial_py_knn_index_cn']]],
+  ['以鼠标为画笔',['以鼠标为画笔',['../d4/d73/tutorial_py_mouse_handling_cn.html',1,'tutorial_py_table_of_contents_gui_cn']]],
+  ['介绍sift（尺度不变特征转换）_5b_5e1_5d',['介绍SIFT（尺度不变特征转换）[^1]',['../d6/d51/tutorial_py_sift_intro_cn.html',1,'tutorial_py_table_of_contents_feature2d_cn']]],
+  ['介绍surf（加速稳健特征）',['介绍SURF（加速稳健特征）',['../db/daf/tutorial_py_surf_intro_cn.html',1,'tutorial_py_table_of_contents_feature2d_cn']]],
+  ['使用svm的手写数据的ocr',['使用SVM的手写数据的OCR',['../d5/d54/tutorial_py_svm_opencv_cn.html',1,'tutorial_py_svm_index_cn']]],
+  ['介绍opencv',['介绍OpenCV',['../d4/d6d/tutorial_py_table_of_contents_setup_cn.html',1,'index']]]
+];
